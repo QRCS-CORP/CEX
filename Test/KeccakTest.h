800bea9cd8bea6c752f2d0a014f3814e5a746d07
Steppenwolfe65 <steppenwolfe_2000@yahoo.com>
2017-12-25 18:24:55 -0500

    1.0.0.5

--- Test/KeccakTest.h
+++ Test/KeccakTest.h
@@ -9,15 +9,7 @@ namespace Test
 	using CEX::Digest::IDigest;
 
 	/// <summary>
-	/// Tests the SHA-3 digest implementation using vector comparisons.
-	/// <para> Using vectors from the Bouncy Castle SHA3 digest and HMAC KAT tests:
-	/// <see href="http://grepcode.com/file/repo1.maven.org/maven2/org.bouncycastle/bcprov-ext-jdk15on/1.51/org/bouncycastle/crypto/test/SHA3DigestTest.java"/>
-	/// Includes vectors from:
-	/// <see href="http://csrc.nist.gov/groups/ST/toolkit/documents/Examples/SHA_All.pdf"/>
-	/// A linking resource:
-	/// <see href="http://www.di-mgt.com.au/sha_testvectors.html"/>
-	/// NIST Secure Hash Standard documentation: 
-	/// <see href="http://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf"/>
+	/// Tests the SHA-3 digest implementation using known vector comparisons
 	/// </summary>
 	class KeccakTest final : public ITest
 	{
@@ -27,21 +19,6 @@ namespace Test
 		static const std::string FAILURE;
 		static const std::string SUCCESS;
 
-		std::vector<std::vector<byte>> m_messages;
-		std::vector<std::vector<byte>> m_expected256;
-		std::vector<std::vector<byte>> m_expected512;
-		std::vector<std::vector<byte>> m_expected1024;
-		std::vector<std::vector<byte>> m_macKeys;
-		std::vector<std::vector<byte>> m_macData;
-		std::vector<std::vector<byte>> m_mac256;
-		std::vector<std::vector<byte>> m_mac512;
-		std::vector<std::vector<byte>> m_mac1024;
-		std::vector<byte> m_truncKey;
-		std::vector<byte> m_truncData;
-		std::vector<byte> m_trunc256;
-		std::vector<byte> m_trunc512;
-		std::vector<byte> m_trunc1024;
-		std::vector<byte> m_xtremeData;
 		TestEventHandler m_progressEvent;
 
 	public:
