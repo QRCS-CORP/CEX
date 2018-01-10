0d416dba45d44d59a7289a30fb73ac44eb66c0a1
Steppenwolfe65 <steppenwolfe_2000@yahoo.com>
2018-01-10 18:33:10 -0500

    1.0.0.4d

--- Test/Misc/Doxygen/html/search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sha3_5ftest_5fstatus',['SHA3_TEST_STATUS',['../sha3__test_8h.html#acffe762eeb4f03e6cd3821f99d970fdd',1,'sha3_test.h']]]
-];
--- Test/Misc/Doxygen/html/search/functions_2.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['sha3_5f256_5fkat_5ftest',['sha3_256_kat_test',['../sha3__test_8h.html#a4f1862cccc147f0747db377d5702442b',1,'sha3_test.h']]],
-  ['sha3_5f512_5fkat_5ftest',['sha3_512_kat_test',['../sha3__test_8h.html#a0dc1fb9acf253a19ef129eda5ab9719c',1,'sha3_test.h']]],
-  ['sha3_5ftest_5frun',['sha3_test_run',['../sha3__test_8h.html#a230f0061c989d8cbbd251366da377fcd',1,'sha3_test.h']]],
-  ['shake_5f128_5fkat_5ftest',['shake_128_kat_test',['../sha3__test_8h.html#a7f6fd441d3c31bba9a275cbe336ca354',1,'sha3_test.h']]],
-  ['shake_5f256_5fkat_5ftest',['shake_256_kat_test',['../sha3__test_8h.html#a2a6d4a0212d9ef6c12bc2f2bcf80d082',1,'sha3_test.h']]]
-];
